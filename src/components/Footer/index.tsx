bf0c1279f4874246d958f74d83c8fc5e12a321cc
Sendy Aditya Suryana <sendzation@gmail.com>
2023-12-16 11:05:35 +0700

    Flat the design

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,16 +3,14 @@ import React from "react";
 
 export const Footer = () => {
   return (
-    <footer className="footer p-10 bg-primary text-primary-content">
+    <footer className="footer p-10">
       <div className="py-16">
         <p className="text-4xl font-bold my-8">
           Interested in working together?
         </p>
-        <a className="daisy-btn daisy-btn-outline daisy-btn-wide daisy-btn-secondary border-white border-solid">
-          <div className="text-white flex flex-row items-center">
+        <a className="button button-primary max-w-full w-96">
             <PaperAirplaneIcon className="w-6 h-6 mr-2" />
             Contact Me
-          </div>
         </a>
       </div>
     </footer>
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -13,14 +13,16 @@ export const HeroComponent = () => {
           <h1 className="text-5xl font-bold">Hello, my name is Sendy!</h1>
           <p>A web frontend developer based in Indonesia, and also build mechanical keyboard in my free time, building things is a drugs for me.</p>
           <p>Currently working at <a href="https://www.gojek.com/" target="_blank">Gojek</a> as Web Frontend Developer, and as designer at <a href="https://www.instagram.com/kbdkspco/" target="_blank">Keyboard Kasep</a> to build ergonomic mechanical keyboards.</p>
-          <span className="daisy-btn daisy-btn-primary mr-2 cursor-default">
+          <div className="flex flex-row gap-2">
+          <span className="button button-primary">
             <CodeBracketSquareIcon className="w-6 h-6 mr-2" />
             Developer
           </span>
-          <span className="daisy-btn daisy-btn-primary mr-2 cursor-default">
+          <span className="button button-primary">
             <WrenchScrewdriverIcon className="w-6 h-6 mr-2" />
             Maker
           </span>
+          </div>
         </div>
       </div>
     </div>
