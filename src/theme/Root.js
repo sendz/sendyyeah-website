6d218dd7bacbb0e8690b81c19eeb90c12c65a090
sendz <sendzation@gmail.com>
2024-07-03 11:31:55 +0700

    Revert

--- src/theme/Root.js
+++ src/theme/Root.js
@@ -3,7 +3,6 @@ import { Analytics } from '@vercel/analytics/react';
 import { CookiesConsent } from "@site/src/components/CookiesConsent";
 import useIsBrowser from "@docusaurus/useIsBrowser";
 import useGtm from "@site/src/hooks/useGtm";
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 // Default implementation, that you can customize
 export default function Root({children}) {
@@ -13,7 +12,6 @@ export default function Root({children}) {
   return <>
     {children}
     <Analytics />
-    <SpeedInsights />
     <CookiesConsent />
   </>;
 }
